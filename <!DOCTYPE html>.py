b9996f73bd4504aa84793498af55e9be044446fc
Sapo <108555872+SapinhoLabasco@users.noreply.github.com>
2022-07-01 20:49:25 -0300

    Add files via upload

---
+++ <!DOCTYPE html>.py
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width,
+initial-scale=1.0">
+<title>sexta-feira do gaúcho..</title>
+</head>
+<style>
+body {
+background-color: rgb(49, 200, 150);}
+h1{
+color:rgb(11, 11, 11)}
+p{
+color:rgb(114, 53, 53)
+font: normal 20pt Arial }
+</style>
+<body>
+<h1>bom dia meu cupincha</h1>
+<p>chimarrão de montão</p>
+</body>
+<script>
+var n1 = number(window.prompt('digite um numero:'))//string
+var n2 =number(window.prompt('digite outro numero:'))//string    
+window.alert('peixinho na brasa')
+window.confirm('kaiser na mão')
+window.prompt('só pra quem pode?')
+</script>
+</html>
